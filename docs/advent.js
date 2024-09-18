65af6b06a98e0aacbf181da591a2949418708682
Deploy <noreply@deployzweb.com>
2024-09-18 05:26:47 +0000

    Deploying on 2024-09-18 05:26:47

--- docs/advent.js
+++ docs/advent.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp_ltyo27t.js
+// include: /tmp/tmprbytmtvi.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -219,21 +219,21 @@ var REMOTE_PACKAGE_SIZE = metadata['remote_package_size'];
 
   })();
 
-// end include: /tmp/tmp_ltyo27t.js
-// include: /tmp/tmpf3u4o8i_.js
+// end include: /tmp/tmprbytmtvi.js
+// include: /tmp/tmpde1m_t48.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpf3u4o8i_.js
-// include: /tmp/tmpx14wkfcr.js
+  // end include: /tmp/tmpde1m_t48.js
+// include: /tmp/tmpbm12apfn.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpx14wkfcr.js
+  // end include: /tmp/tmpbm12apfn.js
 
 
 // Sometimes an existing Module object exists with properties
